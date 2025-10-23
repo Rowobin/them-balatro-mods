4365dd61a89291f8aee4ee10be158bd6a81d37bc
JoaoCerq <joao.a.cerqueira@gmail.com>
2025-10-23 23:58:02 +0100

    Added 2 spells. Sweet Oblivion and Sea Gods Scorn

--- MTGambling/src/spells.lua
+++ MTGambling/src/spells.lua
@@ -104,8 +104,9 @@ SMODS.Consumable {
     loc_txt = {
 		name = 'Funeral Rites',
 		text = {
-            "Draw #1#",
-            "{C:attention}Destroy{} the #1# lowest rank cards in hand",
+            "{C:blue}Draw{} #1#",
+            "{C:attention}Destroy{} the #1# lowest",
+            "rank cards in hand",
             "Lose {C:money}#1#${}"
 		}
 	},
@@ -150,8 +151,8 @@ SMODS.Consumable {
 
     end,
     can_use = function (self, card)
-        if G.hand then
-            return #G.hand.highlighted > 0 and G.GAME.dollars >= G.GAME.bankrupt_at + card.ability.extra.amount
+        if G.hand and G.hand.cards then
+            return G.GAME.blind and G.GAME.blind.chips > 0 and G.GAME.dollars >= G.GAME.bankrupt_at + card.ability.extra.amount
         end
         return false
     end
@@ -201,4 +202,120 @@ SMODS.Consumable {
         return G.hand and #G.hand.highlighted == card.ability.extra.amount
     end
 }
+
+SMODS.Consumable {
+    key = "sweet_oblivion",
+    set = "spells",
+    loc_txt = {
+		name = 'Sweet Oblivion',
+		text = {
+            "{C:attention}Destroy{} the top {C:blue}#1#{}",
+            "cards of the deck"
+		}
+	},
+	atlas = 'TherosBD_spells',
+    pos = {x = 4 , y = 0},
+    config = { extra = { amount = 4 } },
+    loc_vars = function(self, info_queue, card)
+		return { vars = { card.ability.extra.amount } }
+	end,
+    cost = 2,
+    use = function(self, card, area, copier)
+
+        local destroy_cards = {}
+        if G.deck then
+
+            local destroy_amount = card.ability.extra.amount
+            if #G.deck.cards < destroy_amount then
+                destroy_amount = #G.deck.cards
+            end
+
+            for i = 1, card.ability.extra.amount, 1 do
+                destroy_cards[#destroy_cards+1] = G.deck.cards[i]
+            end
+        end
+        
+
+        G.E_MANAGER:add_event(Event({
+            trigger = 'after',
+            delay = 0.4,
+            func = function()
+                play_sound('tarot1')
+                card:juice_up(0.3, 0.5)
+                return true
+            end
+        }))
+
+        G.E_MANAGER:add_event(Event({
+            trigger = 'after',
+            delay = 0.2,
+            func = function()
+                SMODS.destroy_cards(destroy_cards)
+                return true
+            end
+        }))
+
+
+    end,
+    can_use = function (self, card)
+        if G.deck and G.deck.cards then
+            return G.GAME.blind and G.GAME.blind.chips > 0
+        end
+        return false
+    end
+}
+
+local evaluate_play_ref = G.FUNCS.evaluate_play
+
+function G.FUNCS:evaluate_play(e)
+    evaluate_play_ref(e)
+
+    local play_score = G.GAME.chips + math.floor(hand_chips*mult)
+    if (not G.GAME.mtgg_best_hand or G.GAME.mtgg_best_hand.best_score < play_score) then
+        G.GAME.mtgg_best_hand = {best_score = play_score, cards = {}}
+        for _, card in pairs(G.play.cards) do
+            G.GAME.mtgg_best_hand.cards[#G.GAME.mtgg_best_hand.cards+1] = card.unique_val
+        end
+    end
+
+end
+
+SMODS.Consumable {
+    key = "sea_gods_scorn",
+    set = "spells",
+    loc_txt = {
+		name = 'Sea God\'s Scorn',
+		text = {
+            "{C:blue}Draw{} the {C:attention}highest{} score {C:blue}hand{}",
+            "of the previous round"
+		}
+	},
+	atlas = 'TherosBD_spells',
+    pos = {x = 5 , y = 0},
+    cost = 3,
+    use = function(self, card, area, copier)
+
+        local temp_hand = {}
+        for _, playing_card in ipairs(G.GAME.mtgg_best_hand.cards) do temp_hand[#temp_hand + 1] = playing_card end
+
+        for key, deck_card in ipairs(G.deck.cards) do
+            for _, best_card in pairs(temp_hand) do
+                if deck_card.unique_val == best_card then
+                    --[[ print("---- Found highest score card ----")
+                    print(deck_card.base.value)
+                    print(deck_card.base.suit) ]]
+                    draw_card(G.deck,G.hand, 100,'up', true,deck_card)
+                end
+            end
+        end
+
+    end,
+    can_use = function (self, card)
+        if G.hand and G.hand.cards then
+            return G.GAME.blind and G.GAME.blind.chips > 0
+        end
+        return false
+    end
+}
+
 
